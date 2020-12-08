297ed10b43a95befd66a31e699719bd08a4c7688
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2020-12-08 20:38:39 +0530

    Added hash key set capability
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/go-redis/redis/v8 v8.4.2
 	github.com/gomodule/redigo v1.8.3
+	github.com/mitchellh/mapstructure v1.1.2
 	github.com/sirupsen/logrus v1.7.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
--- migrator/migrator.go
+++ migrator/migrator.go
@@ -10,7 +10,7 @@ import (
 // MigrateRedisData is the function to migrate keys from old to new redis
 func MigrateRedisData(redConfig config.Configuration) {
 	for _, database := range redConfig.Databases {
-		logrus.Errorf("Executing migrator for database: %v", database)
+		logrus.Debugf("Executing migrator for database: %v", database)
 		oldRedisClient, err := client.OldRedisClient(redConfig, database)
 		newRedisClient, err := client.NewRedisClient(redConfig, database)
 		if err != nil {
@@ -21,12 +21,38 @@ func MigrateRedisData(redConfig config.Configuration) {
 			logrus.Errorf("Error while listing redis keys %v", err)
 		}
 		for _, key := range keys {
-			value, err := redis.String(oldRedisClient.Do("GET", key))
+			keyType, err := redis.String(oldRedisClient.Do("TYPE", key))
 			if err != nil {
-				logrus.Errorf("Not able to get the value for key %s: %v", key, err)
+				logrus.Errorf("Not able to get the key type %s: %v", key, err)
 			}
-			newRedisClient.Do("SET", key, value)
-			logrus.Debugf("Migrated %s key with value: %v", key, value)
+			switch keyType {
+			case "string":
+				migrateStringKeys(oldRedisClient, newRedisClient, key)
+			case "hash":
+				migrateHashKeys(oldRedisClient, newRedisClient, key)
+			}
 		}
 	}
+}
+
+func migrateHashKeys(oldClient redis.Conn, newClient redis.Conn, key string) {
+	value, err := redis.StringMap(oldClient.Do("HGETALL", key))
+	var data = []interface{}{key}
+	for k, v := range value {
+		data = append(data, k, v)
+	}
+	if err != nil {
+		logrus.Errorf("Not able to get the value for key %s: %v", key, err)
+	}
+	newClient.Do("HMSET", data...)
+	logrus.Debugf("Migrated %s key with value: %v", key, data)
+}
+
+func migrateStringKeys(oldClient redis.Conn, newClient redis.Conn, key string) {
+	value, err := redis.String(oldClient.Do("GET", key))
+	if err != nil {
+		logrus.Errorf("Not able to get the value for key %s: %v", key, err)
+	}
+	newClient.Do("SET", key, value)
+	logrus.Debugf("Migrated %s key with value: %v", key, value)
 }
